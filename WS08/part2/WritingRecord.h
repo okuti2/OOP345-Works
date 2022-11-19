373ab8f0762a213b94531c8a834248b38d4d6329
Olutoyosi Kuti <okuti2@myseneca.ca>
2022-11-19 00:09:24 -0500

    finished implementing

---
+++ WS08/part2/WritingRecord.h
@@ -0,0 +1,25 @@
+/* ------------------------------------------------------
+Workshop 8 part 2
+Module: WritingRecord
+Filename: WritingRecord.h
+Version 1
+Author:	Olutoyosi Kuti; 102633211; okuti2@myseneca.ca
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------
+Date    18th November, 2022
+-----------------------------------------------------------*/
+#ifndef SDDS_WRITINGRECORD_H
+#define SDDS_WRITINGRECORD_H
+
+#include "GeneratingList.h"
+#include "EmpProfile.h"
+
+namespace sdds
+{
+	GeneratingList<EmployeeWage> writeRaw(const GeneratingList<Employee>& emp, const GeneratingList<Salary>& sal);
+	GeneratingList<EmployeeWage> writeSmart(const GeneratingList<Employee>& emp, const GeneratingList<Salary>& sal);
+}
+
+
+#endif // SDDS_WRITINGRECORD_H
+
